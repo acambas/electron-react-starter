61308ca384138f39e93c7173a023dba0d22c61df
acambashmh <aleksander.cambas@hmhco.com>
2016-05-18 16:48:56 +0100

    added loading css

--- app/client/containers/mainPage/index.js
+++ app/client/containers/mainPage/index.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 import React from 'react';
 
 class component extends React.Component {
     render() {
-        return <div>
-           zxc 
+        return <div className="btn btn-default">
+           zxc  asd
         </div>
     }
 }
