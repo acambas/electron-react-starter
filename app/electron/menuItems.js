7f9c3e72c5c676539ea2ee698474876da35ce13f
acambashmh <aleksander.cambas@hmhco.com>
2016-05-19 13:45:37 +0100

    setup basic right click menu items

---
+++ app/electron/menuItems.js
@@ -0,0 +1,33 @@
+const {remote} = require('electron');
+const { Menu, MenuItem} = remote;
+
+
+const menu =  Menu.buildFromTemplate([{
+        label: 'Undo',
+        role: 'undo',
+    }, {
+        label: 'Redo',
+        role: 'redo',
+    }, {
+        type: 'separator',
+    }, {
+        label: 'Cut',
+        role: 'cut',
+    }, {
+        label: 'Copy',
+        role: 'copy',
+    }, {
+        label: 'Paste',
+        role: 'paste',
+    }, {
+        type: 'separator',
+    }, {
+        label: 'Select all',
+        role: 'selectall',
+    },
+]);
+
+window.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    menu.popup(remote.getCurrentWindow());
+}, false);
